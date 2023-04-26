363d76dd780633e3e6817fc79cebe8174583ab24
korzck <konstantin.koretskiy@gmail.com>
2023-04-26 22:22:04 +0300

    testing gin load in vercel

--- api/index.go
+++ api/index.go
@@ -12,19 +12,24 @@ var(
 
 )
 
-func f(c *gin.Context){
-	c.String(http.StatusOK,"Hello from golang in vercel")
+func admin(c *gin.Context){
+	c.String(http.StatusOK,"Hello from admin page of trainhop")
 }
 
-func myRoute(r *gin.RouterGroup){
-	r.GET("/admin", f)
+func root(c *gin.Context){
+	c.String(http.StatusOK,"Hello from root page of trainhop!")
 }
+
+// func myRoute(r *gin.RouterGroup){
+// 	r.GET("/admin", f)
+// }
 
 func init(){
 	App = gin.New()
-	r := App.Group("/api")
-	myRoute(r)
-
+	// r := App.Group("/api")
+	// myRoute(r)
+	App.GET("/admin", admin)
+	App.GET("/", root)
 }
 
 func Handler(w http.ResponseWriter , r *http.Request){
